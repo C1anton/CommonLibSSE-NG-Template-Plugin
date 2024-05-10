6421d327a48d3b73f67631b10ca9dada6548d39b
Patchu1i <frostedproglide@proton.me>
2024-05-10 13:29:40 -0500

    Convert CMakeLists

--- cmake/headerlist.cmake
+++
@@ -1,9 +0,0 @@
-# Provide CMAKE with a list of .h header files, source files are handled in "cmake/sourcelist.cmake"
-
-set(headers ${headers}
-        src/PCH.h
-    src/hook.h
-    src/log.h
-    src/settings.h
-    src/util.h
-)
--- cmake/sourcelist.cmake
+++
@@ -1,6 +0,0 @@
-# Provide CMAKE with a list of .cpp source files. Header files are listed in "cmake/headerlist.cmake"
-
-set(sources ${sources}
-    src/hook.cpp
-    src/plugin.cpp
-)
